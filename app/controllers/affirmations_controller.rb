2936ea46fbeb450568a996e88e05197e92bc53a7
makmemeng <yatani.mems4568@gmail.com>
2021-10-16 17:36:57 +0900

    create affirmation

---
+++ app/controllers/affirmations_controller.rb
@@ -0,0 +1,51 @@
+class AffirmationsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+  def index
+    @affirmations = Affirmation.includes(:user).order(id: "DESC")
+  end
+
+  def show
+    @affirmation = Affirmation.find(params[:id])
+  end
+
+  def new
+    @affirmation = Affirmation.new
+  end
+
+  def create
+    @affirmation = Affirmation.new(affirmation_params)
+    @affirmation.user_id = current_user.affirmations.build(affirmation_params)
+    if @affirmation.save
+      redirect_to affirmation_path(@affirmation), notice: '投稿に成功しました。'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @affirmation = Affirmation.find(params[:id])
+    if @affirmation.user != current_user
+      redirect_to affirmations_path, alert: '不正なアクセスです。'
+    end
+  end
+
+  def update
+    @affirmation = Affirmation.find(params[:id])
+    if @affirmation.update(affirmation_params)
+      redirect_to affirmation_path(@affirmation), notice: '更新に成功しました。'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    affirmation = Affirmation.find(params[:id])
+    affirmation.destroy
+    redirect_to user_path(affirmation.user)
+  end
+
+  private
+  def affirmation_params
+    params.require(:affirmation).permit(:title, :body, :image)
+  end
+end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '69a8a4a76253ba91226ea04fd98c329a2d7d908a7a84ec083e30b50bc9daf27232fcbc27a8697b2cb4fa7bf6d064c2b4ae8e3f89ddfebd1eeb727d385caa1229'
