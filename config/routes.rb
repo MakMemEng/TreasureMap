0073e94d6dd2a69c3795734e6ee0f0c9a4ffd04e
makmemeng <yatani.mems4568@gmail.com>
2021-10-26 20:27:08 +0900

    fix guest login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: "home#index"
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
   resources :users
   resources :affirmations
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
 end
