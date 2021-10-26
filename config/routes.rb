1f49ce6a777791aecb03f9a91f79ab199c583029
makmemeng <yatani.mems4568@gmail.com>
2021-10-26 17:45:42 +0900

    Add Admin user

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :admins, controllers: {
-    sessions:      'admins/sessions',
-  }
-  namespace :admins do
-    resources :users, only: %i(index destroy)
-    resources :affirmations, only: %i(index destroy)
-  end
   devise_for :users
   root to: "home#index"
   resources :users
