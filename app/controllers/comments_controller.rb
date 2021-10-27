22f8953fd4ab7a769cbafb4e977d5a1451502639
makmemeng <yatani.mems4568@gmail.com>
2021-10-27 15:13:14 +0900

    Create Comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,27 @@
 class CommentsController < ApplicationController
+  def index
+    @comments = Comment.includes(:user).order(id: "DESC")
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    redirect_to affirmation_path(comment.user)
+  end
 end
