139e37154c94ad4b4c6507d233dd9f3999623392
makmemeng <yatani.mems4568@gmail.com>
2021-10-29 14:51:51 +0900

    Add Pagenation for kaminari

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,27 +1,24 @@
 class CommentsController < ApplicationController
-  def index
-    @comments = Comment.includes(:user).order(id: "DESC")
-  end
-
-  def show
-    @comment = Comment.find(params[:id])
-  end
-
-  def new
-    @comment = Comment.new
-  end
+  before_action :authenticate_user!, only: [:create, :destroy]
 
   def create
-
-  end
-
-  def edit
-
+    @affirmation = Affirmation.find(params[:affirmation_id])
+    @comment = @affirmation.comments.new(comment_params)
+    @comment.user_id = current_user.id
+    @comments = @affirmation.comments.all.includes(:user).order(created_at: :desc)
+    @comment.save
+    render :create
   end
 
   def destroy
-    comment = Comment.find(params[:id])
+    comment = Comment.find(id: params[:id], affirmation_id: params[:affirmation_id])
     comment.destroy
+    render :destroy
     redirect_to affirmation_path(comment.user)
   end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:comment, :affirmation_id)
 end
