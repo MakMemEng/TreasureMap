5f252705480768e09764c11a1d41aa388b3b6dc4
makmemeng <yatani.mems4568@gmail.com>
2021-10-30 14:53:15 +0900

    単体テスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,11 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   validates :username,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
-                                              format: { with: VALID_EMAIL_REGEX }
-	def self.guest
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+  validates :profile,  length: { maximum: 300 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
+  def self.guest
 		find_or_create_by!(username: 'Guest_User',email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
     end
