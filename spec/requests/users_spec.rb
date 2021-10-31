ce9a82b1d55a45b9fde1cd9833f1d1b3440158f8
makmemeng <yatani.mems4568@gmail.com>
2021-10-31 13:14:51 +0900

    user 単体テスト

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,24 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/users/index"
-      expect(response).to have_http_status(:success)
+  let(:user) { create(:user) }
+
+  describe '#' do
+    it '正常にアクセスできること' do
+      get users_path(user)
+      expect(response).to have_http_status(200)
     end
   end
 
-  describe "GET /show" do
-    it "returns http success" do
-      get "/users/show"
-      expect(response).to have_http_status(:success)
+  describe '#show' do
+    it '正常にアクセスできること' do
+      get user_path(user)
+      expect(response).to have_http_status(302)
     end
   end
 
-  describe "GET /edit" do
-    it "returns http success" do
-      get "/users/edit"
-      expect(response).to have_http_status(:success)
+  describe '#edit' do
+    it '正常にアクセスできること' do
+      get edit_user_path(user)
+      expect(response).to have_http_status(302)
+    end
+  end
+
+  describe '#guest' do
+    it '正常にアクセできること' do
+      get users_guest_sign_in_path
+      expect(response).to have_http_status(302)
     end
   end
 
