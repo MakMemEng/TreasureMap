57647548eea3820651dba98792d03d6e4d86c16a
makmemeng <yatani.mems4568@gmail.com>
2021-10-31 13:19:32 +0900

    comment 単体テスト

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:comment) { create(:comment) }
+
+  describe '#create' do
+    context '保存できる場合' do
+      it '入力が正しいこと' do
+        expect(comment).to be_valid
+      end
+
+      it '1文字以上であれば保存できること' do
+        comment.comment = 'a' * 1
+        expect(comment).to be_valid
+      end
+
+      it '200文字以内であれば保存できること' do
+        comment.comment = 'a' * 200
+        expect(comment).to be_valid
+      end
+    end
+
+    context '保存できない場合' do
+      it '入力が空欄だと保存できない' do
+        comment.comment = ''
+        expect(comment).to be_invalid
+      end
+
+      it '201文字以上だと保存できない' do
+        comment.comment = 'a' * 201
+        expect(comment).to be_invalid
+      end
+
+      it 'affirmation_idがnilだと保存できない' do
+        comment.affirmation_id = nil
+        expect(comment).to be_invalid
+      end
+
+      it 'user_idがnilだと保存できない' do
+        comment.user_id = nil
+        expect(comment).to be_invalid
+      end
+    end
+  end
+
 end
