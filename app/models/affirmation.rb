03226701286228dbee74e709d17415cc0d91cf8a
makmemeng <yatani.mems4568@gmail.com>
2021-11-08 14:13:04 +0900

    Fix bug

--- app/models/affirmation.rb
+++ app/models/affirmation.rb
@@ -2,11 +2,12 @@ class Affirmation < ApplicationRecord
   belongs_to :user
   has_many  :comments,  dependent: :destroy
   attachment :image
-  validates :title,     presence: true, length: { maximum: 30 }
-  validates :body,      presence: true, length: { maximum: 400 }
-  validates :image,     presence: true
-  validates :image_id,  presence: true
 
-  validates :user_id,   presence: true
+  with_options presence: true do
+    validates :title,     length: { maximum: 30 }
+    validates :body,      length: { maximum: 400 }
+    validates :image
+    validates :user_id
+  end
 
 end
